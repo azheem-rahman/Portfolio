2d69bed430e48b4ad3d8ef42d2ecb2450e4a2818
Azheem Rahman <azheem.rahman@gmail.com>
2023-02-02 11:31:47 +0800

    added skeletal layout for landing page component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,28 @@
 import React from "react";
+import LandingPageProfilePhoto from "../assets/LandingPageProfilePhoto.jpg";
 
 const LandingPage = () => {
   return (
     <div>
-      <h1>Landing Page Here</h1>
+      <div
+        className="container"
+        style={{ border: "1px solid rgba(0, 0, 0, 0.05)" }}
+      >
+        <div className="row">
+          <div className="col">
+            <h1>Hi there!</h1>
+            <h1>I'm Azheem, an aspiring software engineer.</h1>
+            <h2>Welcome to my Portfolio.</h2>
+          </div>
+          <div className="col d-flex justify-content-center">
+            <img
+              src={LandingPageProfilePhoto}
+              width={200}
+              alt="landing-page-profile-photo"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
