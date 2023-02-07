f704c5ce3bd192eac4529d93aaecfe1c055a28d0
Azheem Rahman <azheem.rahman@gmail.com>
2023-02-07 16:46:12 +0800

    edited font size for LandingPage component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,18 +3,18 @@ import LandingPageProfilePhoto from "../assets/LandingPageProfilePhoto.jpg";
 
 const LandingPage = () => {
   return (
-    <div>
+    <div id="landing-page">
       <div
         className="container d-flex align-items-center"
         style={{ height: "80vh" }}
       >
         <div className="row align-items-center">
           <div className="col text-end">
-            <h2>Hi there!</h2>
-            <h2>
+            <h4>Hi there!</h4>
+            <h4>
               I'm Azheem, a recent software engineering bootcamp graduate.
-            </h2>
-            <h2>Welcome to my Portfolio.</h2>
+            </h4>
+            <h4>Welcome to my Portfolio.</h4>
           </div>
           <div className="col justify-content-center">
             <img
