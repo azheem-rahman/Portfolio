f3b41087475779fc9233b7d34cb049f012579285
Azheem Rahman <azheem.rahman@gmail.com>
2023-02-07 14:44:22 +0800

    added resume to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
+      <Navbar variant="dark" expand="sm">
+        <Container className="align-middle">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">
               <Nav.Link>Home</Nav.Link>
               <Nav.Link>About Me</Nav.Link>
-              <Nav.Link>Portfolio</Nav.Link>
-              <Nav.Link>Contact</Nav.Link>
+              <Nav.Link>Projects</Nav.Link>
+              <Nav.Link href="https://drive.google.com/file/d/1OCmNJwYmBL99OXM_wobIMT-L7jkQvyE8/view?usp=sharing">
+                <Button variant="outline-light" size="sm">
+                  Resume
+                </Button>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
