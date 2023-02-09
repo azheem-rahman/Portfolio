698d3ec5f997337bf9a87fec5b6eb7fc6f1efd28
Azheem Rahman <azheem.rahman@gmail.com>
2023-02-09 11:00:48 +0800

    minor edit layout LandingPage component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,15 +6,17 @@ const LandingPage = () => {
     <div id="landing-page">
       <div
         className="container d-flex align-items-center"
-        style={{ height: "80vh" }}
+        style={{ height: "90vh" }}
       >
         <div className="row align-items-center">
           <div className="col text-end">
-            <h4>Hi there!</h4>
-            <h4>
-              I'm Azheem, a recent software engineering bootcamp graduate.
-            </h4>
-            <h4>Welcome to my Portfolio.</h4>
+            <h3>Hi there!</h3>
+            <h5>
+              I'm <span style={{ backgroundColor: "#2E5A88" }}>Azheem</span>, a
+              recent software engineering bootcamp graduate with a new found
+              passion in coding.
+            </h5>
+            <h5>Welcome to my Portfolio.</h5>
           </div>
           <div className="col justify-content-center">
             <img
