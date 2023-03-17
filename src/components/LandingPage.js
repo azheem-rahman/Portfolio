62c4293363381b46f246024a1d0a86c27e49ef73
Azheem Rahman <azheem.rahman@gmail.com>
2023-03-17 19:20:55 +0800

    added typing animation and removed image to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,31 +1,23 @@
 import React from "react";
 import LandingPageProfilePhoto from "../assets/LandingPageProfilePhoto.jpg";
 
+import styles from "./LandingPage.module.css";
+
 const LandingPage = () => {
   return (
     <div id="landing-page">
-      <div
-        className="container d-flex align-items-center"
-        style={{ height: "90vh" }}
-      >
-        <div className="row align-items-center">
-          <div className="col text-end">
-            <h3>Hi there!</h3>
+      <div className={styles.container}>
+        <div className="row">
+          <div className="col">
+            <div className={styles.typewriter}>
+              <h3>Hi there!</h3>
+            </div>
             <h5>
               I'm <span style={{ backgroundColor: "#2E5A88" }}>Azheem</span>, a
               recent software engineering bootcamp graduate with a new found
               passion in coding.
             </h5>
             <h5>Welcome to my Portfolio.</h5>
-          </div>
-          <div className="col justify-content-center">
-            <img
-              src={LandingPageProfilePhoto}
-              className="img-fluid"
-              width={200}
-              alt="landing-page-profile-photo"
-              style={{ borderRadius: "10px" }}
-            />
           </div>
         </div>
       </div>
